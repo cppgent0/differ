fbcfffcf2f40b8a2850d5211f962d1df9df0cdd7
John Arrizza <cppgent0@gmail.com>
2016-07-14 08:31:08 -0700

    add prawn formatter

--- lib/differ.rb
+++ lib/differ.rb
@@ -3,6 +3,7 @@ require 'differ/diff'
 require 'differ/format/ascii'
 require 'differ/format/color'
 require 'differ/format/html'
+require 'differ/format/prawn'
 
 module Differ
   class << self
@@ -49,6 +50,7 @@ module Differ
       when :ascii then Format::Ascii
       when :color then Format::Color
       when :html  then Format::HTML
+      when :prawn then Format::Prawn
       when nil    then nil
       else raise "Unknown format type #{f.inspect}"
       end
